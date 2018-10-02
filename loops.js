079c5abc01021987f89349c8292547ed4ae6ff28
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 18:54:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,37 @@
-function forLoop(array){
-  for (var i=0;i <25;i++){
-    log.console(`I am ${i} strange loops`)
+function forLoop(array1){
+  for (let i=0;i <25;i++){
+    if (i === 1){
+    array1.push("I am " + i + " strange loop.")
+  }else{
+    array1.push("I am " + i + " strange loops.")
   }
+  }
+  return array1
+}
+
+function whileLoop(num){
+  while (num >0){
+    console.log(num);
+    num = num -1 }
+    return 'done'
+}
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array){
+  do{
+    delete array.pop();
+  }while(array.length > 0 && incrementVariable());
+  
+  return array;
 }
